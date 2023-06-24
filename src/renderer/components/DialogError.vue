728a0403562729d4d2152e5aeb8abe977d9e392f
ArtiomInc <artiom.game@gmail.com>
2023-06-24 09:03:24 +0200

    Build last version

--- src/renderer/components/DialogError.vue
+++ src/renderer/components/DialogError.vue
@@ -15,7 +15,7 @@ export default {
 </script>
 
 <template>
-  <div class="block fixed w-screen h-screen z-50 top-0 left-0 bg-black/50">
+  <div class="block fixed w-screen h-screen z-50 top-0 left-0 bg-black/50 dark:bg-white/25">
     <div class="flex items-start mt-20 justify-center w-screen h-screen">
       <div class="bg-white dark:bg-neutral-800 p-2 rounded-lg drop-shadow">
         <span v-html="content"></span>
